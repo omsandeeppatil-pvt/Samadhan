14aca76e4fb8cff946c2718ead4cca272fe316b2
Om Patil <159125383+omsandeeppatil-pvt@users.noreply.github.com>
2026-09-01 14:52:07 +0000

    Disable ESLint blocking production builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Allows production builds to succeed even with ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
